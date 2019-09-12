f5d52ea23eae18ab8a796b7e7a44f7fe9cbc52c8
twsthomas <twsthomas@gmail.com>
2019-09-12 13:01:26 +0200

    use dim

--- main_script.py
+++ main_script.py
@@ -4,16 +4,17 @@ from threadpoolctl import threadpool_limits
 
 import argparse
 
-print('Hello')
+# python3 main_script.py test low --cpu 10
 parser = argparse.ArgumentParser()
 parser.add_argument("folder", help="train or test")
 parser.add_argument("dim", help="low or high")
 parser.add_argument("--cpu", help="cpu limits",
                     default=10, type=int)
 args = parser.parse_args()
-# print(args.dim)
 
 max_cpu = args.cpu
+print('°'*20)
+print('running main script with:')
 print("folder=", args.folder)
 print("dim=", args.dim)
 print('max cpu=',max_cpu)
@@ -23,16 +24,14 @@ if args.folder == "train":
         print('* * '*10)
         print('Run main_train id:', id)
         print('* * '*10)
-        with threadpool_limits(limits=max_cpu):
-            run_main_train(id)
+        run_main_train(id, dim=args.dim)
 
 elif args.folder == "test":
     for id in range(1,9):
         print('* * '*10)
         print('Run main_test id:', id)
         print('* * '*10)
-        with threadpool_limits(limits=max_cpu):
-            run_main_test(id)
+        run_main_test(id, dim=args.dim)
 
 else:
     raise args.folder + " received, but should be train or test"
