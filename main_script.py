31a3d2af6ac86481c2d3ba06ce142fead8c524f9
twsthomas <twsthomas@gmail.com>
2019-09-12 12:51:24 +0200

    maxc_cpu

--- main_script.py
+++ main_script.py
@@ -8,18 +8,22 @@ print('Hello')
 parser = argparse.ArgumentParser()
 parser.add_argument("folder", help="train or test")
 parser.add_argument("dim", help="low or high")
+parser.add_argument("--cpu", help="cpu limits",
+                    default=10, type=int)
 args = parser.parse_args()
 # print(args.dim)
 
+max_cpu = args.cpu
 print("folder=", args.folder)
 print("dim=", args.dim)
+print('max cpu=',max_cpu)
 
 if args.folder == "train":
     for id in range(1,3201):
         print('* * '*10)
         print('Run main_train id:', id)
         print('* * '*10)
-        with threadpool_limits(limits=10):
+        with threadpool_limits(limits=max_cpu):
             run_main_train(id)
 
 elif args.folder == "test":
@@ -27,7 +31,7 @@ elif args.folder == "test":
         print('* * '*10)
         print('Run main_test id:', id)
         print('* * '*10)
-        with threadpool_limits(limits=10):
+        with threadpool_limits(limits=max_cpu):
             run_main_test(id)
 
 else:
